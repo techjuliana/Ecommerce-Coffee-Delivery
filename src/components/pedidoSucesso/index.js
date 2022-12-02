9f52945f713aab709b571df7f1333bcfe3588373
Juliana Bitencourt <julianasouzabittencourt@gmail.com>
2022-12-02 14:36:14 -0300

    pedido sucesso styled

--- src/components/pedidoSucesso/index.js
+++ src/components/pedidoSucesso/index.js
@@ -1,17 +1,43 @@
-import { Container } from "./styled";
+import {
+  Container,
+  ContainerTexto,
+  Lista,
+  Subtitulo,
+  Titulo,
+  Conjunto,
+} from "./styled";
 import men from "./../../assets/men.svg";
 import { FaMapMarkerAlt } from "react-icons/fa";
 import { RiTimerFill } from "react-icons/ri";
 import { MdOutlineAttachMoney } from "react-icons/md";
+import { Informacao } from "../about/styled";
 export default function PedidoSucesso() {
   return (
     <Container>
-      <h1>Uhu! Pedido confirmado</h1>
-      <h2>Agora é só aguardar que logo o café chegará até você</h2>
-      <FaMapMarkerAlt />
-      <RiTimerFill />
-      <MdOutlineAttachMoney />
-      <img src={men} alt="" />
+      <ContainerTexto>
+        <Conjunto>
+          <Titulo>Uhu! Pedido confirmado</Titulo>
+          <Subtitulo>
+            Agora é só aguardar que logo o café chegará até você
+          </Subtitulo>
+          <Lista>
+            <Informacao>
+              <FaMapMarkerAlt className="circulo1" />
+              Entrega em Rua João Daniel Martinelli, 102 Farrapos - Porto
+              Alegre, RS
+            </Informacao>
+            <Informacao>
+              <RiTimerFill className="circulo1" /> Previsão de entrega 20 min -
+              30 min
+            </Informacao>
+            <Informacao>
+              <MdOutlineAttachMoney className="circulo1" />
+              Pagamento na entrega Cartão de Crédito
+            </Informacao>
+          </Lista>
+        </Conjunto>
+        <img src={men} alt="" />
+      </ContainerTexto>
     </Container>
   );
 }
--- src/components/pedidoSucesso/styled.js
+++ src/components/pedidoSucesso/styled.js
@@ -1,3 +1,71 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const ContainerTexto = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  @media (max-width: 800px) {
+    flex-direction: column;
+    text-align: center;
+  }
+  .circulo4 {
+    border-radius: 50%;
+    width: 25px;
+    height: 25px;
+    padding: 4px;
+    margin-right: 0.6rem;
+    background: ${(props) => props.theme.lilas2};
+    color: ${(props) => props.theme.body};
+  }
+
+  .circulo1 {
+    border-radius: 50%;
+    width: 25px;
+    height: 25px;
+    padding: 5px;
+    margin-right: 0.6rem;
+    background: ${(props) => props.theme.laranja1};
+    color: ${(props) => props.theme.body};
+  }
+`;
+
+export const Container = styled.div`
+  margin: 1rem;
+`;
+
+export const Lista = styled.div`
+  border: 1px solid #8047f8;
+  border-radius: 8px 50px 8px 50px;
+  display: grid;
+  padding: 2rem;
+  @media (max-width: 800px) {
+    grid-template-columns: 100%;
+  }
+`;
+
+export const Conjunto = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Titulo = styled.h1`
+  width: 25rem;
+  @media (max-width: 800px) {
+    width: 100%;
+  }
+`;
+
+export const Informacao = styled.text`
+  margin-bottom: 1rem;
+`;
+
+export const Subtitulo = styled.text`
+  width: 33rem;
+  font-size: 1.2rem;
+  margin-bottom: 2.5rem;
+  margin-top: 1rem;
+  color: ${(props) => props.theme.subtitulo};
+  @media (max-width: 800px) {
+    width: 100%;
+  }
+`;
